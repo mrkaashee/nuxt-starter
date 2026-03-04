43760d79993b59d6fa5e47157931f450b6834238
mrkaashee <mrkaashee@gmail.com>
2026-03-04 16:21:40 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,23 @@ export default defineNuxtConfig({
     '@nuxt/hints',
     'nuxt-auth-utils',
   ],
+  $production: {
+    nitro: {
+      preset: 'cloudflare_module',
+      cloudflare: {
+        deployConfig: true, nodeCompat: true,
+        wrangler: { observability: { enabled: true } }
+      }
+    },
+    hub: {
+      db: {
+        dialect: 'sqlite',
+        driver: 'd1',
+        connection: { databaseId: process.env.DATABASE_ID }
+      },
+    }
+  },
+
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   ui: {
@@ -20,11 +37,9 @@ export default defineNuxtConfig({
     defaults: { nuxtLink: { trailingSlash: 'remove' } },
     viteEnvironmentApi: true, typescriptPlugin: true, extractAsyncDataHandlers: true
   },
-  compatibilityDate: '2026-02-25',
-  nitro: {
-    preset: 'cloudflare_module',
-    cloudflare: { deployConfig: true, nodeCompat: true }
-  },
+  compatibilityDate: '2026-03-04',
+
   hub: { db: 'sqlite' },
+
   eslint: { config: { stylistic: true } },
 })
