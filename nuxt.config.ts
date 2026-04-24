af64b324c1a52aa0bd389f25a7a7df3c5b5a834c
mrkaashee <mrkaashee@gmail.com>
2026-04-24 11:21:53 +0500

    removed eslint and updated dep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,10 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/hints',
-    '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxthub/core'
   ],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
-  eslint: { config: { stylistic: true } },
 })
