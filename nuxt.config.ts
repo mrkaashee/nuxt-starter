d9dcf439a3376e85c9dc1222e6c918c467c81914
mrkaashee <mrkaashee@gmail.com>
2026-05-13 16:06:08 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-07-15",
+
+  $development: {},
+  hub: {
+    db: { dialect: "sqlite", casing: "snake_case" },
+    // kv: {},
+    // cache: {},
+    // blob: {},
+  },
 })
