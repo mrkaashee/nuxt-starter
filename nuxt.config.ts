9b379f3eea134cb900bf2fd030fe76f7bb019ac5
mrkaashee <mrkaashee@gmail.com>
2026-07-30 11:17:26 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxthub/core"],
+  modules: ["@nuxt/ui", "@nuxthub/core", "@nuxt/hints"],
   css: ["~/assets/css/main.css"],
-  compatibilityDate: "2026-07-12",
+  compatibilityDate: "2026-07-20",
   devtools: { enabled: true, timeline: { enabled: true } },
+
+  tracingChannel: true,
   experimental: {
+    prefetchPreloadTags: true,
+    watcher: "builder",
     typedPages: true,
     writeEarlyHints: true,
     defaults: { nuxtLink: { trailingSlash: "remove" } },
--- vite.config.ts
+++ vite.config.ts
@@ -22,10 +22,28 @@ export default defineConfig({
       attributes: ["class", "ui"],
       functions: ["clsx", "cn", "cva", "tw", "defineAppConfig"],
     },
+    sortPackageJson: {
+      sortScripts: true,
+    },
   },
   lint: {
+    categories: {
+      // correctness: "error",
+      // nursery: "warn",
+      // pedantic: "warn",
+      // perf: "warn",
+      // restriction: "warn",
+      // style: "warn",
+      // suspicious: "warn",
+    },
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
-    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    rules: {
+      "capitalized-comments": "off",
+      curly: "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
+      "vite-plus/prefer-vite-plus-imports": "error",
+    },
     options: { typeAware: true, typeCheck: true },
   },
 })
